0d647a2cd678254196c42ef80df2f86c3260d46e
Mark Santolucito <msantolucuito13@gmail.com>
2011-10-29 16:05:56 -0400

    added a html popup

--- background.js
+++ background.js
@@ -2,19 +2,19 @@ var patt = new RegExp("facebook.com");
 var pagesVisited = 0;
 function checkTab(tabId, tab) {
     
-    if(patt.test(tab.url)){
-	 pagesVisited++;
-    }
-    if(patt.test(tab.url)&&pagesVisited>=7){
+    if(patt.test(tab.url)&&pagesVisited>=3){
     	chrome.tabs.remove(tabId);
-	alert("You visited "+pagesVisited+" pages on FB! \n Get back to work!"); 
+	window.open("popup.html", "FB",
+	"status = 1, height = 300, width = 400, location = no, menubar = no, titlebar=no"); 
 	pagesVisited=0;
     }
-    
+        if(patt.test(tab.url)){
+	 pagesVisited++;
+    }
 }
 
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab){
-    if (info.status == "complete"){
+    if (tab.url !== undefined && info.status == "complete"){
 	checkTab(tabId, tab);
     }
 });
