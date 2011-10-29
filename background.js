d4f2e5480dc3bbac1919f1620b14b4f52d214485
Mark Santolucito <msantolucuito13@gmail.com>
2011-10-29 15:10:53 -0400

    redo background.js

--- background.js
+++ background.js
@@ -1,18 +1,22 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var patt = new RegExp("facebook.com");
+var pagesVisited = 0;
+function checkTab(tabId, tab) {
+    
+    if(patt.test(tab.url)){
+	 pagesVisited++;
+    }
+    if(patt.test(tab.url)&&pagesVisited>=7){
+    	chrome.tabs.remove(tabId);
+	alert("You visited "+pagesVisited+" pages on FB! \n Get back to work!"); 
+	pagesVisited=0;
+    }
+    
+}
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(
-      null, {code:"document.body.style.background='red !important'"});
-	chrome.tabs.remove(5);
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab){
+    if (info.status == "complete"){
+	checkTab(tabId, tab);
+    }
 });
 
 
-
-var i = 0;
-window.setInterval(function() {
-  chrome.browserAction.setBadgeText({text:String(i)});
-  i++;
-}, 600);
